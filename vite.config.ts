a1a28adb2196d6ae57c5b00a8c0a6d6c012676b6
Sofi Bel <slipway.beds_0m@icloud.com>
2023-11-29 19:50:17 +0300

    add projects section

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-
 export default defineConfig({
     root: resolve(__dirname, "src/"),
+    publicDir: resolve(__dirname, "public/"),
     build: {
         outDir: resolve(__dirname, "dist/"),
         emptyOutDir: true,
