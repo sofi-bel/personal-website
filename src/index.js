5e24f2ed04fc74b95d687add23f55eb8d369c994
Sofi Bel <slipway.beds_0m@icloud.com>
2023-12-14 18:11:55 +0300

    change navbar layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const navbarToggler = document.querySelector(".navbar__toggler");
-const navbar = document.querySelector(".navbar");
-const navbarNav = document.querySelector(".navbar__nav");
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarToggler = document.querySelector(".navbar__toggler");
+  const navbar = document.querySelector(".navbar");
+  const navbarNav = document.querySelector(".navbar__nav");
 
-navbarToggler.addEventListener("click", handleNavClick);
-navbarNav.addEventListener("click", handleNavClick);
+  navbarToggler.addEventListener("click", () => {
+    toggleTwoClasses(navbar, "nav-is-visible", "nav-is-hidden", 500);
+    toggleTwoClasses(navbarNav, "nav-is-visible", "nav-is-hidden", 500);
+  });
+});
 
-function handleNavClick() {
-  navbar.classList.toggle("nav-opened");
-  navbarNav.classList.toggle("nav-opened");
+function toggleTwoClasses(element, first, second, timeOfAnimation) {
+  if (!element.classList.contains(first)) {
+    element.classList.add(first);
+    element.classList.remove(second);
+  } else {
+    element.classList.add(second);
+    window.setTimeout(function () {
+      element.classList.remove(first);
+    }, timeOfAnimation);
+  }
 }
