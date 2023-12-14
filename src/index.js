9823631063754fce1ee4574b29460994894c440c
Sofi Bel <slipway.beds_0m@icloud.com>
2023-12-14 18:59:47 +0300

    add dark mode

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const navbarToggler = document.querySelector(".navbar__toggler");
-  const navbar = document.querySelector(".navbar");
-  const navbarNav = document.querySelector(".navbar__nav");
-
-  navbarToggler.addEventListener("click", () => {
-    toggleTwoClasses(navbar, "nav-is-visible", "nav-is-hidden", 500);
-    toggleTwoClasses(navbarNav, "nav-is-visible", "nav-is-hidden", 500);
-  });
-});
-
 function toggleTwoClasses(element, first, second, timeOfAnimation) {
   if (!element.classList.contains(first)) {
     element.classList.add(first);
@@ -20,3 +9,63 @@ function toggleTwoClasses(element, first, second, timeOfAnimation) {
     }, timeOfAnimation);
   }
 }
+
+function checkDarkModeSettings() {
+  const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    btnDarkMode.classList.add("dark-mode-btn_active");
+    document.body.classList.add("page_theme_dark");
+  }
+
+  if (localStorage.getItem("darkMode") === "dark") {
+    btnDarkMode.classList.add("dark-mode-btn_active");
+    document.body.classList.add("page_theme_dark");
+  } else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn_active");
+    document.body.classList.remove("page_theme_dark");
+  }
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      const newColorScheme = event.matches ? "dark" : "light";
+
+      if (newColorScheme === "dark") {
+        btnDarkMode.classList.add("dark-mode-btn_active");
+        document.body.classList.add("page_theme_dark");
+        localStorage.setItem("darkMode", "dark");
+      } else {
+        btnDarkMode.classList.remove("dark-mode-btn_active");
+        document.body.classList.remove("page_theme_dark");
+        localStorage.setItem("darkMode", "light");
+      }
+    });
+
+  btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn_active");
+    const isDark = document.body.classList.toggle("page_theme_dark");
+
+    if (isDark) {
+      localStorage.setItem("darkMode", "dark");
+    } else {
+      localStorage.setItem("darkMode", "light");
+    }
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarToggler = document.querySelector(".navbar__toggler");
+  const navbar = document.querySelector(".navbar");
+  const navbarNav = document.querySelector(".navbar__nav");
+
+  navbarToggler.addEventListener("click", () => {
+    toggleTwoClasses(navbar, "nav-is-visible", "nav-is-hidden", 500);
+    toggleTwoClasses(navbarNav, "nav-is-visible", "nav-is-hidden", 500);
+  });
+
+  checkDarkModeSettings();
+});
