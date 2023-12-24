ef7fae8c974d524462c5028e3446b190aa27e9c6
Sofi Bel <slipway.beds_0m@icloud.com>
2023-12-24 15:18:40 +0300

    add files for React app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Header from "./components/header/Header";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/footer/Footer";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectPage from "./pages/ProjectPage";
+import ContactsPage from "./pages/ContactsPage";
+
+import ScrollToTop from "./utils/scrollToTop";
+
+import "./assets/styles/index.scss";
+
+function App() {
+  return (
+    <div className="App page">
+      <Router>
+        <ScrollToTop />
+        <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/project/:id" element={<ProjectPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
