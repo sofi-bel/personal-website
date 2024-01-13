caefc88a042adbdd2e0135063712c96ab188c19d
Sofi Bel <slipway.beds_0m@icloud.com>
2024-01-13 21:26:04 +0300

    add animation for hero header

--- src/components/heroHeader/HeroHeader.jsx
+++ src/components/heroHeader/HeroHeader.jsx
@@ -6,12 +6,13 @@ const HeroHeader = () => {
     <header className="hero-header">
       <div className="hero-header__wrapper">
         <h1 className="hero-header__title title title_size_xxl">
-          Hi, my name is Sofi Bel.
-          <br />
-          <strong className="hero-header__title hero-header__title_typing title title_accent">
-            I&apos;m a Front End Developer
-          </strong>
+          Hi, my name is<br></br>Sofi&nbsp;Bel.
         </h1>
+        <h2>
+          <strong className="hero-header__title hero-header__title_typing title title_accent">
+            I&apos;m a Frontend Developer
+          </strong>
+        </h2>
         <Btn
           link="https://hh.ru/resume/3c29fb55ff0bdfe6c30039ed1f534445353343"
           text="Download CV"
