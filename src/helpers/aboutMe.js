6ed1bd742de4bd7e757b279963f8f2057fa215e8
Sofi Bel <slipway.beds_0m@icloud.com>
2024-03-22 13:24:17 +0300

    update About Me

--- src/helpers/aboutMe.js
+++ src/helpers/aboutMe.js
@@ -12,12 +12,12 @@ const aboutMe = [
     srcImageBigWebp: SofiBelLWebp,
     imageAlt: "Sofi Bel image",
     description:
-      "Frontend developer with over 3 years of experience in web development. My " +
+      "Frontend developer with over 2 years of experience in web development. My " +
       "responsibilities include creating, supporting, and testing websites, as well " +
       "as developing user interfaces with a focus on cross-browser and mobile" +
       " compatibility (mobile-first approach). I possess skills in HTML, Pug," +
       " CSS, SASS/SCSS, JavaScript (ES6+), TypeScript, React, React-Router," +
-      " and React Hooks. My unique advantage lies in my continuous pursuit of " +
+      " React Hooks and Redux. My unique advantage lies in my continuous pursuit of " +
       "learning and improvement.  Additionally, I am proactive and adept at planning " +
       "my own activities to achieve maximum results.",
   },
